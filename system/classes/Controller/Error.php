a512bf2f1867912cbee8649923e17b32ae730370
andygoo <i@jiesc.net>
2015-11-07 13:48:24 +0800

    Signed-off-by: andygoo <i@jiesc.net>

--- system/classes/Controller/Error.php
+++ system/classes/Controller/Error.php
@@ -3,7 +3,15 @@
 class Controller_Error extends Controller {
 
     public function action_404() {
-        echo '404';
+        echo <<<EOF
+<html>
+<head><title>404 Not Found</title></head>
+<body bgcolor="white">
+<center><h1>4o4 Not Found</h1></center>
+<hr><center>nginx</center>
+</body>
+</html>
+EOF;
         exit;
     }
 }

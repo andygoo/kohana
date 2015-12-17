a538329b844bad422bbcd531cc63327f0f2c7e03
andygoo <i@jiesc.net>
2015-12-17 20:53:33 +0800

    Signed-off-by: andygoo <i@jiesc.net>

--- system/classes/Function.php
+++ system/classes/Function.php
@@ -20,6 +20,15 @@ function parse_query2($url) {
     return $arr;
 }
 
+function array_filter_recursive($input) {
+    foreach ($input as &$value) {
+        if (is_array($value)) {
+            $value = array_filter_recursive($value);
+        }
+    }
+    return array_filter($input);
+}
+
 function create_uuid() {
     return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x', 
         mt_rand(0, 0xffff), 

3950e37105e65bf59c5e30356a9ce47e29d74456
andygoo <i@jiesc.net>
2016-01-21 11:19:11 +0800

    Signed-off-by: andygoo <i@jiesc.net>

--- modules/weixin/init.php
+++ modules/weixin/init.php
@@ -1,6 +1,6 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-Route::set('weixin_oauth', 'weixin(/<controller>(/<action>))', array('directory' => 'weixin', 'controller' => '(oauth|reply)'))
+Route::set('weixin_oauth', '<directory>(/<controller>(/<action>))', array('directory' => 'weixin', 'controller' => '(oauth|reply)'))
 ->defaults(array(
 'controller' => 'oauth',
 'action'     => 'index',

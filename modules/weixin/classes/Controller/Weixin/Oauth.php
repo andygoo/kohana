4d99af165cb5955b2ca3721c436270dd382f459a
andygoo <i@jiesc.net>
2016-01-21 11:30:27 +0800

    Signed-off-by: andygoo <i@jiesc.net>

--- modules/weixin/classes/Controller/Weixin/Oauth.php
+++ modules/weixin/classes/Controller/Weixin/Oauth.php
@@ -6,7 +6,8 @@ class Controller_Weixin_Oauth extends Controller {
         $wx = new WeixinOauth();
         $user_info = $wx->get_user_info();
         if (empty($user_info)) {
-            $this->redirect('weixin/oauth/login');
+            $callback_url = URL::site('weixin/oauth', true);
+            $this->redirect('weixin/oauth/login?callback_url=' . urlencode($callback_url));
         }
         var_dump($user_info);
         exit;
@@ -14,7 +15,7 @@ class Controller_Weixin_Oauth extends Controller {
     
     public function action_login() {
         $wx = new WeixinOauth();
-        $callback_url = URL::site('weixin/oauth', true);
+        $callback_url = $_GET['callback_url'];
         $login_url = $wx->get_login_url($callback_url);
         $this->redirect($login_url);
     }

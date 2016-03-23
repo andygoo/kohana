ebae90688361ad97cd31699f037572b79777e21c
andygoo <i@jiesc.net>
2016-03-23 11:15:45 +0800

    Signed-off-by: andygoo <i@jiesc.net>

--- system/classes/Function.php
+++ system/classes/Function.php
@@ -295,3 +295,14 @@ function get_parents_ids($arr, $id) {
     }
     return $ret;
 }
+
+function base64url_encode($data, $pad = null) {
+    $data = str_replace(array('+', '/'), array('-', '_'), base64_encode($data));
+    if (!$pad) {
+        $data = rtrim($data, '=');
+    }
+    return $data;
+}
+function base64url_decode($data) {
+    return base64_decode(str_replace(array('-', '_'), array('+', '/'), $data));
+}

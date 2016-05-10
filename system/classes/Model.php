3191fa7f02a77387c1165a3188e8dab5859d63d9
andygoo <i@jiesc.net>
2016-05-10 21:27:57 +0800

    Signed-off-by: andygoo <i@jiesc.net>

--- system/classes/Model.php
+++ system/classes/Model.php
@@ -41,8 +41,8 @@ class Model {
     }
     
     public function select($offset, $size, $where = null, $columns = '*') {
-        $where['LIMIT'] = " $offset, $size ";
-        return $this->db->select($this->_table_name, $columns, $this->where_clause($where));
+        //$where['LIMIT'] = " $offset, $size ";
+        return $this->db->select($this->_table_name, $columns, $this->where_clause($where) . " LIMIT $offset, $size");
     }
 
     public function getAll($where = null, $columns = '*') {

216df115dc3dd1fc58ad667e13d32ed984730f98
andygoo <i@jiesc.net>
2016-05-26 15:15:16 +0800

    Signed-off-by: andygoo <i@jiesc.net>

--- modules/admin/views/index.php
+++ modules/admin/views/index.php
@@ -1,2 +1,53 @@
 
+<?= HTML::script('media/js/Sortable.min.js')?>
+
 <h3 class="page-header">首页</h3>
+
+<div class="row" id="pannel">
+  <div class="col-xs-6">
+    <div class="panel panel-success">
+        <div class="panel-heading">
+            <h3 class="panel-title">Panel title1</h3>
+        </div>
+        <div class="panel-body">
+            Panel content
+        </div>
+    </div>
+  </div>
+  <div class="col-xs-6">
+    <div class="panel panel-info">
+        <div class="panel-heading">
+            <h3 class="panel-title">Panel title2</h3>
+        </div>
+        <div class="panel-body">
+            Panel content
+        </div>
+    </div>
+  </div>
+  <div class="col-xs-6">
+    <div class="panel panel-warning">
+        <div class="panel-heading">
+            <h3 class="panel-title">Panel title3</h3>
+        </div>
+        <div class="panel-body">
+            Panel content
+        </div>
+    </div>
+  </div>
+  <div class="col-xs-6">
+    <div class="panel panel-danger">
+        <div class="panel-heading">
+            <h3 class="panel-title">Panel title4</h3>
+        </div>
+        <div class="panel-body">
+            Panel content
+        </div>
+    </div>
+  </div>
+</div>
+
+<script>
+var pannel = document.getElementById("pannel");
+Sortable.create(pannel, {handle: ".panel-title",});
+</script>
+

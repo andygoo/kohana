bd48f72c6098f82574d3f8bd863a88fdc0bc1402
andygoo <i@jiesc.net>
2016-05-26 15:28:54 +0800

    Signed-off-by: andygoo <i@jiesc.net>

--- modules/admin/views/index.php
+++ modules/admin/views/index.php
@@ -4,7 +4,7 @@
 <h3 class="page-header">首页</h3>
 
 <div class="row" id="pannel">
-  <div class="col-xs-6">
+  <div class="col-xs-12 col-sm-6">
     <div class="panel panel-success">
         <div class="panel-heading">
             <h3 class="panel-title">Panel title1</h3>
@@ -14,7 +14,7 @@
         </div>
     </div>
   </div>
-  <div class="col-xs-6">
+  <div class="col-xs-12 col-sm-6">
     <div class="panel panel-info">
         <div class="panel-heading">
             <h3 class="panel-title">Panel title2</h3>
@@ -24,7 +24,7 @@
         </div>
     </div>
   </div>
-  <div class="col-xs-6">
+  <div class="col-xs-12 col-sm-6">
     <div class="panel panel-warning">
         <div class="panel-heading">
             <h3 class="panel-title">Panel title3</h3>
@@ -34,7 +34,7 @@
         </div>
     </div>
   </div>
-  <div class="col-xs-6">
+  <div class="col-xs-12 col-sm-6">
     <div class="panel panel-danger">
         <div class="panel-heading">
             <h3 class="panel-title">Panel title4</h3>
@@ -48,6 +48,19 @@
 
 <script>
 var pannel = document.getElementById("pannel");
-Sortable.create(pannel, {handle: ".panel-title",animation: 150});
+Sortable.create(pannel, {
+	handle: ".panel-title",
+	animation: 150,
+	store: {
+    	get: function (sortable) {
+    		var order = localStorage.getItem(sortable.options.group);
+    		return order ? order.split('|') : [];
+    	},
+    	set: function (sortable) {
+    		var order = sortable.toArray();
+    		localStorage.setItem(sortable.options.group, order.join('|'));
+    	}
+    }
+});
 </script>
 

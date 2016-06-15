4bf21b7ddd4796b66f61c4aada092a69693affa8
andygoo <i@jiesc.net>
2016-06-15 19:03:41 +0800

    Signed-off-by: andygoo <i@jiesc.net>

--- modules/weixin/classes/Controller/Weixin/Reply.php
+++ modules/weixin/classes/Controller/Weixin/Reply.php
@@ -3,9 +3,10 @@
 class Controller_Weixin_Reply extends Controller {
 
     public function action_index() {
-        $token = 'wxtest';
-        $appid = 'wxc5b1d86df49a2dc4';
-        $encodingAesKey = '';
+        $wx_config = Kohana::config('weixin.default');
+        $token = $wx_config['token'];
+        $appid = $wx_config['appid'];
+        $encodingAesKey = $wx_config['encodingAesKey'];
 
         $wechat = new MyWechat($token, $encodingAesKey, $appid);
         $wechat->run();

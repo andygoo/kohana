39c109d8cda26208f6f38b10a1667af1a03c0b21
andygoo <i@jiesc.net>
2016-06-22 19:02:51 +0800

    Signed-off-by: andygoo <i@jiesc.net>

--- system/classes/Controller/Error.php
+++ system/classes/Controller/Error.php
@@ -2,7 +2,22 @@
 
 class Controller_Error extends Controller {
 
+    public function action_403() {
+        header('HTTP/1.1 403 Forbidden');
+        echo <<<EOF
+<html>
+<head><title>403 Forbidden.</title></head>
+<body bgcolor="white">
+<center><h1>403 Forbidden</h1></center>
+<hr><center>nginx</center>
+</body>
+</html>
+EOF;
+        exit;
+    }
+    
     public function action_404() {
+        header("HTTP/1.1 404 Not Found");
         echo <<<EOF
 <html>
 <head><title>404 Not Found.</title></head>

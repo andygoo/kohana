ffcdc5f0358c4698d5f6ae6f3b3df41e73fba21e
andygoo <i@jiesc.net>
2016-06-23 14:35:20 +0800

    Signed-off-by: andygoo <i@jiesc.net>

--- modules/sso/init.php
+++
@@ -1,6 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-
-Route::set('ssoserver', 'sso/<action>/<broker>/<token>/<checksum>')->defaults(array('controller'=>'sso'));
-
-SSO::init();

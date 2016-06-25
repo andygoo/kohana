6cdb421167ed3f8ebc89e3d787562250ecab5dcb
andygoo <i@jiesc.net>
2016-06-25 10:19:30 +0800

    Signed-off-by: andygoo <i@jiesc.net>

--- modules/sms/init.php
+++
@@ -1,7 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-Route::set('sms', 'sms/<action>', array('action' => '(send)'))
-	->defaults(array(
-		'controller' => 'sms',
-		'action' => 'send',
-		));

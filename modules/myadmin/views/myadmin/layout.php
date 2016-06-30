e010d38a60531b64faca9bc0c71a30f6dec63a7d
andygoo <i@jiesc.net>
2016-06-30 14:05:46 +0800

    Signed-off-by: andygoo <i@jiesc.net>

--- modules/myadmin/views/myadmin/layout.php
+++ modules/myadmin/views/myadmin/layout.php
@@ -6,8 +6,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Myadmin</title>
 <?= HTML::style('media/bootstrap/css/bootstrap.min.css')?>
-<?= HTML::style('media/css/dashboard.css')?>
 <?= HTML::style('media/offcanvas/css/bootstrap.offcanvas.css')?>
+<?= HTML::style('media/css/dashboard.css')?>
 <style>
 .sidebar{padding-top:0px; }
 .navbar-inverse{background: #3c8dbc;}
@@ -45,7 +45,6 @@
 <?= HTML::script('media/js/jquery.min.js')?>
 <?= HTML::script('media/bootstrap/js/bootstrap.min.js')?>
 <?= HTML::script('media/offcanvas/js/bootstrap.offcanvas.js')?>
-<?= HTML::script('media/js/bootstrap-notify.min.js')?>
 </head>
 <body>
 
@@ -53,7 +52,7 @@
 	<div class="container-fluid">
 		<div class="navbar-header">
 			<a class="navbar-brand" href="<?= URL::site('myadmin')?>"><i class="glyphicon glyphicon-home"></i>&nbsp;phpMyAdmin</a>
-            <button type="button" class="navbar-toggle offcanvas-toggle pull-right" data-toggle="offcanvas" data-target="#js-bootstrap-offcanvas" style="float:left;">
+            <button type="button" class="navbar-toggle offcanvas-toggle" data-toggle="offcanvas" data-target="#js-bootstrap-offcanvas">
                 <span class="sr-only">Toggle navigation</span>
                 <span>
                   <span class="icon-bar"></span>

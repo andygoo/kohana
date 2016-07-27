c3a2e2acbc1e8836c641e147b32de6c51d899d4d
andygoo <i@jiesc.net>
2016-07-27 17:41:38 +0800

    Signed-off-by: andygoo <i@jiesc.net>

--- modules/media/init.php
+++ modules/media/init.php
@@ -1,6 +1,6 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-Route::set('media', 'media(/<action>)/<file>.<format>', array('file'=>'[a-zA-Z0-9\.\-_/]+', 'format'=>'[a-zA-Z0-9]+'))
+Route::set('media', 'media(/<action>)/<file>.<format>', array('action'=>'(index|minicss|minijs)', 'file'=>'[a-zA-Z0-9\.\-_/]+', 'format'=>'[a-zA-Z0-9]+'))
 	->defaults(array(
 		'controller' => 'media',
 		'action'     => 'index',

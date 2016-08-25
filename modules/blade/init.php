4483cf1d6464dd617da369260cbe83e72f1ad444
andygoo <i@jiesc.net>
2016-08-25 17:27:51 +0800

    Signed-off-by: andygoo <i@jiesc.net>

--- modules/blade/init.php
+++ modules/blade/init.php
@@ -3,5 +3,5 @@
 // Check if the blade directory exists in cache,
 // if not then create 
 if (!file_exists(APPPATH.'cache/blade')) {
-   mkdir(APPPATH.'cache/blade', 0755);
+   mkdir(APPPATH.'cache/blade', 0755, true);
 }
